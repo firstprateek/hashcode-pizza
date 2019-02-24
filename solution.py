f5dd4bcaff4440629f1bc7135cc4cc4d21f6ea04
Prateek Joshi <lastprateek@gmail.com>
2019-02-24 00:44:09 -0600

    expansion on top

--- solution.py
+++ solution.py
@@ -8,6 +8,7 @@ class Solution:
     self.h = h
     self.pizza = pizza
     self.lower_item = 'X'
+    self.higher_item = 'X'
     self.lower_item_count = 0
     self.max_slices = 0
     self.total_score = 0
@@ -18,26 +19,53 @@ class Solution:
     size = 1
     corners = [ (row, col), (row, col), (row, col), (row, col) ] # top - left, top - right, bottom - right, bottom - left
     cur_row, cur_col = row, col
+    
+    count = { 'T': 0, 'M': 0 }
+    count[self.lower_item] += 1
+    self.pizza[row] = self.pizza[row][:col] + 'X' + self.pizza[row][col + 1:]
 
-    while any(expansion) and size < h:
+    while any(expansion) and size < self.h:
       for direction in expansion:
         if not direction:
           continue
 
-        border_check = { 0: cur_row == 0, 1: cur_col == c - 1, 2: cur_row == r - 1, 3: cur_col == 0 }
+        cur_boundry = { 
+          0: corners[0][0], 1: corners[1][1], 2: corners[2][1], 3: corners[3][1] 
+        }
+        border_check = { 
+          0: cur_boundry[0] == 0, 
+          1: cur_boundry[1] == self.c - 1, 
+          2: cur_boundry[2] == self.r - 1, 
+          3: cur_boundry[3] == 0 
+        }
+
         if border_check[direction]:
           expansion[direction] = False
           continue
 
         if direction == 0:
+          # increase the row and then check if lower_item count satisfies
+          new_row = pizza[corners[0][0] - 1][corners[0][1]:corners[1][1]]
+          new_row_lower_items, new_row_higher_items = new_row.count(self.lower_item), new_row.count(self.higher_item)
+          if count[self.lower_item] + new_row_lower_items > self.l or sum(count.values()) + len(new_row) > h:
+            expansion[0] = False
+            continue
 
-          corners[0] = (corners[0][0], corners[0][1] - 1)
+          count[self.lower_item] += new_row_lower_items
+          count[self.higher_item] += new_row_higher_items
+
+          # update corners
+          corners[0] = (corners[0][0] - 1, corners[0][1])
+          corners[1] = (corners[1][0] - 1, corners[1][1])
         elif direction == 1:
 
         elif direction == 2:
 
         elif direction == 3:
+          # if less than l then take it
 
+    # check if lower item counts satisfy
+    # return slice coordinates
 
 
   def solve(self):
@@ -49,10 +77,11 @@ class Solution:
       number_of_T += row.count('T')
       number_of_M += row.count('M')
 
-    self.lower_item = 'T'
+    self.lower_item, self.higher_item = 'T', 'M'
     self.lower_item_count = number_of_T
     if number_of_M < number_of_T:
       self.lower_item, self.lower_item_count = 'M', number_of_M
+      self.higher_item = 'T'
 
     print('lower_item: {},\t lower_item_count: {}\n'.format(self.lower_item, self.lower_item_count))
     # ----- ----- ----- ----- ----- ----- -----
