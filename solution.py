8972fc0131c1e1010ca4abbdbdcaeb3969a42fcc
Prateek Joshi <lastprateek@gmail.com>
2019-02-23 23:47:29 -0600

    make Solution class

--- solution.py
+++ solution.py
@@ -1,72 +1,84 @@
 import sys
 
-def make_slice(row, col, r, c, l, h, pizza):
-  expansion = [ True, True, True, True] # Top, Right, Bottom, Left
-  size = 1
-  corners = [ (row, col), (row, col), (row, col), (row, col) ] # top - left, top - right, bottom - right, bottom - left
-  cur_row, cur_col = row, col
+class Solution:
+  def __init__(self, r, c, l, h, pizza):
+    self.r = r
+    self.c = c
+    self.l = l
+    self.h = h
+    self.pizza = pizza
+    self.lower_item = 'X'
+    self.lower_item_count = 0
+    self.max_slices = 0
+    self.total_score = 0
+    self.slices = []
 
-  while any(expansion) and size < h:
-    for direction in expansion:
-      if not direction:
-        continue
+  def make_slice(self, row, col):
+    expansion = [ True, True, True, True] # Top, Right, Bottom, Left
+    size = 1
+    corners = [ (row, col), (row, col), (row, col), (row, col) ] # top - left, top - right, bottom - right, bottom - left
+    cur_row, cur_col = row, col
 
-      border_check = { 0: cur_row == 0, 1: cur_col == c - 1, 2: cur_row == r - 1, 3: cur_col == 0 }
-      if border_check[direction]:
-        expansion[direction] = False
-        continue
+    while any(expansion) and size < h:
+      for direction in expansion:
+        if not direction:
+          continue
 
-      if direction == 0:
+        border_check = { 0: cur_row == 0, 1: cur_col == c - 1, 2: cur_row == r - 1, 3: cur_col == 0 }
+        if border_check[direction]:
+          expansion[direction] = False
+          continue
 
-        corners[0] = (corners[0][0], corners[0][1] - 1)
-      elif direction == 1:
+        if direction == 0:
 
-      elif direction == 2:
+          corners[0] = (corners[0][0], corners[0][1] - 1)
+        elif direction == 1:
 
-      elif direction == 3:
+        elif direction == 2:
+
+        elif direction == 3:
 
 
 
-def solve(r, c, l, h, pizza):
-  print("Solving for:\nr: {},\tc: {},\tl: {},\th: {}\npizza: {}\n".format(r, c, l, h, pizza))
+  def solve(self):
+    print("Solving for:\nr: {},\tc: {},\tl: {},\th: {}\npizza: {}\n".format(r, c, l, h, pizza))
 
-  # Find limiting ingredient T or M -----
-  number_of_T, number_of_M = 0, 0
-  for row in pizza:
-    number_of_T += row.count('T')
-    number_of_M += row.count('M')
+    # Find limiting ingredient T or M -----
+    number_of_T, number_of_M = 0, 0
+    for row in pizza:
+      number_of_T += row.count('T')
+      number_of_M += row.count('M')
 
-  lower_item = 'T'
-  lower_item_count = number_of_T
-  if number_of_M < number_of_T:
-    lower_item, lower_item_count = 'M', number_of_M
+    self.lower_item = 'T'
+    self.lower_item_count = number_of_T
+    if number_of_M < number_of_T:
+      self.lower_item, self.lower_item_count = 'M', number_of_M
 
-  print('lower_item: {},\t lower_item_count: {}\n'.format(lower_item, lower_item_count))
-  # ----- ----- ----- ----- ----- ----- -----
+    print('lower_item: {},\t lower_item_count: {}\n'.format(self.lower_item, self.lower_item_count))
+    # ----- ----- ----- ----- ----- ----- -----
 
-  max_slices = lower_item_count // l
+    self.max_slices = lower_item_count // l
 
-  slices = []
-  # step 1 find a lower_item
-  for row in range(r):
-    for col in range(c):
-      if pizza[row][col] == 'X':
-        continue
+    # step 1 find a lower_item
+    for row in range(r):
+      for col in range(c):
+        if pizza[row][col] == 'X':
+          continue
 
-      if pizza[row][col] == lower_item:
-        print('found {} at {}:{}'.format(lower_item, row, col))
-        slices.append(make_slice(row, col, r, c, l, h, pizza))
+        if pizza[row][col] == lower_item:
+          print('found {} at {}:{}'.format(lower_item, row, col))
+          self.slices.append(self.make_slice(row, col))
 
-  # step 2 exapnd around lower item until you satisfy l
-  # mark areas already cut
-  # try to maximise and reach h in a slice
-  '''
-  -----------
- | TM |T| TT |
- | TM |M| MT |
- | TT |T| TT |
-  -----------
-  '''
+    # step 2 exapnd around lower item until you satisfy l
+    # mark areas already cut
+    # try to maximise and reach h in a slice
+    '''
+    -----------
+   | TM |T| TT |
+   | TM |M| MT |
+   | TT |T| TT |
+    -----------
+    '''
 if __name__ == "__main__":
   # Parse input and create pizza matrix -----
   try:
@@ -80,5 +92,10 @@ if __name__ == "__main__":
     input_data = f.readlines()
 
   # ----- ----- ----- ----- ----- ----- -----
-  
-  solution = solve(*[ int(data.strip()) for data in input_data[0].split(' ')], input_data[1:])
+
+  solution = Solution(*[ int(data.strip()) for data in input_data[0].split(' ')], input_data[1:])
+  result = solution.solve()
+
+  # Write result to output file -----
+
+  # ----- ----- ----- ----- ----- ----- -----
