2930d42a78cea2dd66d051176fffe1e2ccb6d2f2
Prateek Joshi <lastprateek@gmail.com>
2019-02-25 20:09:15 -0600

    gitignore changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -a input=("a_example.in" "b_small.in" "c_medium.in" "d_big.in")
 
-for file_name in "${input[@]}"
+for file_name in input/*.in
 do
-  python3 solution.py "$file_name" "output_$file_name"
+  python3 solution.py "$file_name" "./output/output_$file_name"
 done
